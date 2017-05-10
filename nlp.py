17b719ca0feb2bf37bc7b79c42d79970d7351bf0
Jad Nohra <jadnohra@users.noreply.github.com>
2017-05-10 21:46:57 +0200

    .

--- nlp.py
+++ nlp.py
@@ -1,4 +1,5 @@
 import sys, os, re
+import requests
 
 if '-no_spacy' not in sys.argv:
 	print ' Loading spacy..',; sys.stdout.flush();
@@ -6,10 +7,72 @@ if '-no_spacy' not in sys.argv:
 	nlp = spacy.load('en')
 	print '.'
 
+def fpjoin(aa):
+	ret = os.path.join(aa[0], aa[1])
+	for a in aa[2:]:
+		ret = os.path.join(ret,a)
+	return ret
+def fphere():
+	return os.path.dirname(os.path.realpath(__file__))
+def fpjoinhere(aa):
+	return fpjoin([fphere()]+aa)
+def fptemp():
+	return fpjoin([fphere(), 'temp'])
+def cwdtemp():
+	os.chdir(fptemp())
+def mktemp():
+	if os.path.isdir(fptemp()) == False:
+		os.mkdir(fptemp())
+def randfilename(dir, pre, ext):
+	i = 1
+	while True:
+		fname = "{}{}.{}".format(pre, i, ext)
+		if (os.path.isfile(os.path.join(dir, fname)) == False):
+			return fname
+		i=i+1
+
 def unistr(strg):
 	if not isinstance(strg, unicode):
 		return unicode(strg, "utf-8")
 	return strg
+
+def find_word_type(word):
+	def clean_word(strng):
+		ret = ''.join(ch for ch in strng.strip() if ch.isalnum() or ch in ['-']).lower()
+		return '' if any(x.isdigit() for x in ret) else ret
+	def is_valid_path(path):
+		return os.path.exists(path) or os.access(os.path.dirname(path), os.W_OK)
+	word = clean_word(word)
+	dict_temp = os.path.join(fptemp(), 'dict')
+	mktemp()
+	if os.path.isdir(dict_temp) == False:
+		os.mkdir(dict_temp)
+	word_cache_path = os.path.join(dict_temp, 'cache_{}.txt'.format(word))
+	is_cacheable = is_valid_path(word_cache_path)
+	has_cache = os.path.exists(word_cache_path)
+	if is_cacheable and (has_cache == False):
+		site = 'en.wiktionary.org'
+		site = 'en.wikipedia.org'
+		url = 'https://{}/w/api.php?action=query&titles={}&prop=extracts&format=json'.format(site, word)
+		print ' querying [{}]'.format(word),; sys.stdout.flush();
+		resp = requests.post(url)
+		print '.',
+		with open(word_cache_path, 'w') as fo:
+			fo.write(resp.text)
+	if os.path.exists(word_cache_path):
+		resp_text = ''
+		with open(word_cache_path, 'r') as fi:
+			resp_text = fi.read().lower()
+		word_type = ''
+		if any(x in resp_text for x in ['mathematics', 'mathematical']):
+			word_type = 'math'
+		elif 'The requested page title contains invalid characters'.lower() in resp_text:
+			word_type = ''
+		elif '"missing":""' in resp_text:
+			word_type = ''
+		else:
+			word_type = 'nonmath'
+	return word_type
 
 def parse(text):
 	#print ' Parsing ..',; sys.stdout.flush();
@@ -71,7 +134,8 @@ def main():
 						#	print '[{}.]'.format(def_sent)
 			#print all_words['(resp'], def_all_words['(resp']; sys.exit(0);
 			if True:
-				def_all_words = dict([kv for kv in def_all_words.items() if kv[0] in all_words])
+				#def_all_words = dict([kv for kv in def_all_words.items() if kv[0] in all_words and all_words[kv[0]] > 100 ])
+				def_all_words = dict([kv for kv in sorted(def_all_words.items(), key = lambda x: x[0]) if kv[0] in all_words and all_words[kv[0]] > 20 and find_word_type(kv[0]) != 'math' ]); print '';
 			def_all_words_lst = sorted(def_all_words.items(), key = lambda x: -x[1])
 			#print len(def_all_words_lst), def_all_words_lst[:20]
 			#def_words_in_all_words = [(kv[0], max(all_words.get(kv[0], -1), kv[1]) ) for kv in def_all_words.items()]
@@ -112,12 +176,14 @@ def main():
 					print def_all_words['qf'], def_words_in_all_words['qf'];
 					movers = sorted([(x, count_sort_in_def.index(x)-count_sort_in_all.index(x) ) for x in def_all_words], key=lambda x: -x[1])
 					print movers[:20]
-			if False:
+			if True:
 				ratios_in_def = dict([(kv[0], float(kv[1])/total_in_def) for kv in def_all_words.items()])
 				ratios_in_all = dict([(kv[0], float(kv[1])/total_in_all) for kv in def_words_in_all_words.items()])
 				ratio_diff_map = [ (x, ratios_in_def[x]/ratios_in_all[x]) for x in def_all_words.keys() if (x in ratios_in_def and x in ratios_in_all) ]
 				ratio_diff_map = sorted(ratio_diff_map, key = lambda x: -x[1])
-				print [ (x, def_all_words[x[0]], all_words.get(x[0], -1)) for x in ratio_diff_map[:20]]
+				print ''
+				print [ (x, def_all_words[x[0]], all_words.get(x[0], -1)) for x in ratio_diff_map[:100]]
+				print ''
 		if False:
 			for mi, match in enumerate(pat_matches['def-sent']):
 				def_sent = match.groups()[-1].replace('\n', ' ').strip()
