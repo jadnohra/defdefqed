7831004db28c869f7a2da35c9428a292e1c56724
Jad Nohra <jadnohra@users.noreply.github.com>
2017-05-28 23:58:13 +0200

    Added purifier to wikisql

--- wikisqltest.py
+++ wikisqltest.py
@@ -1,4 +1,4 @@
-import traceback, sys, os, os.path, requests, unidecode, subprocess
+import traceback, sys, os, os.path, requests, unidecode, subprocess, copy
 import pymysql
 
 g_dbg = '-dbg' in sys.argv
@@ -43,6 +43,12 @@ def vt_col_code(col):
 
 k_wikioff_db = 'enwiki-20170520'.replace('-', '_')
 k_wikioff_db_usr = 'root'
+g_wikioff_total_queries = 0
+
+def wikioff_qry_exec(cur, cmd):
+	global g_wikioff_total_queries
+	g_wikioff_total_queries = g_wikioff_total_queries + 1
+	cur.execute(cmd)
 
 def wikioff_get_cat_edges(cur, cat_title, get_parents=True, get_children=True):
 	children = []
@@ -50,10 +56,10 @@ def wikioff_get_cat_edges(cur, cat_title, get_parents=True, get_children=True):
 	cmd_children = "select P.page_title from page as P, categorylinks as CL where CL.cl_to='{}' and CL.cl_type='subcat' and P.page_id=CL.cl_from and P.page_namespace=14".format(cat_title.replace("'", "\\'"))
 	cmd_parents = "select CL.cl_to from page as P, categorylinks as CL where P.page_title='{}' and P.page_namespace=14 and CL.cl_from=P.page_id and CL.cl_type='subcat'".format(cat_title.replace("'", "\\'"))
 	if get_children:
-		cur.execute(cmd_children)
+		wikioff_qry_exec(cur, cmd_children)
 		children.extend([x[0] for x in cur.fetchall()])
 	if get_parents:
-		cur.execute(cmd_parents)
+		wikioff_qry_exec(cur, cmd_parents)
 		parents.extend([x[0] for x in cur.fetchall()])
 	return (parents, children)
 
@@ -69,19 +75,19 @@ def wikioff_cat_graph_down(cur, cat_title, max_depth, verbose=False, progress=Fa
 			sys.stdout.write('\x1B[2K'); sys.stdout.write('\r'); sys.stdout.write(' {}'.format(info_str)); sys.stdout.flush();
 		return edges(node)
 	def recurse():
-		while len(recurse_subs) > max_depth:
+		if len(recurse_subs) > max_depth:
 			return
 		next_subs = []
 		for sub in recurse_subs[-1]:
 			sub_children = wikioff_get_cat_edges(cur, sub, False, True)[1]
 			for sub_child in sub_children:
-				edges(sub)[sub_child] = ''
+				edges(sub)[sub_child] = ''; edges(sub_child);
 			next_subs.extend(sub_children)
 		if verbose:
 			print '---{}---\n'.format(len(recurse_subs)),next_subs,'\n'
 		recurse_subs.append(next_subs)
 		recurse()
-	recurse_subs.append([cat_title])
+	recurse_subs.append([cat_title]); edges(cat_title);
 	recurse()
 	if progress and not verbose:
 		print ' (done)'
@@ -101,19 +107,60 @@ def wikioff_graph_node_purity(cur, graph, cat_title):
 	graph_children = graph[cat_title]
 	graph_parents = extract_parents(graph, cat_title)
 	diff_parents = list_diff(cat_parents, graph_parents)
-	diff_children = list_diff(cat_children, graph_children)
-	return (diff_parents, diff_children)
+	is_leaf = (len(graph[cat_title]) == 0)
+	if is_leaf:
+		diff_children = []
+	else:
+		diff_children = list_diff(cat_children, graph_children)
+	return (diff_parents, diff_children, 'leaf' if is_leaf else '')
 
 def wikioff_graph_node_is_pure(cur, graph, cat_title):
-	diff_parents, diff_children = wikioff_graph_node_purity(cur, graph, cat_title)
+	diff_parents, diff_children, is_leaf = wikioff_graph_node_purity(cur, graph, cat_title)
 	return len(diff_parents) + len(diff_children) == 0
 
-def wikioff_graph_pure_nodes(cur, graph):
-	ret = []
+def wikioff_graph_root_paths(graph, root):
+	paths = {}
+	def recurse(path, node):
+		if node in path:
+			return #loop
+		new_path = copy.copy(path); new_path.append(node);
+		if node not in paths:
+			paths[node] = []
+		paths[node].append(new_path);
+		for child in graph[node]:
+			recurse(new_path, child)
+	recurse([], root)
+	return paths
+
+def wikioff_graph_pure_nodes(cur, graph, cat_title, allow_impure_parents, progress = False):
+	pure = []
+	pure.append(cat_title)
 	for i,k in enumerate(sorted(graph.keys())):
+		if progress:
+			info_str = '{}/{}. [{}]...'.format(i+1, len(graph), k)
+			sys.stdout.write('\x1B[2K'); sys.stdout.write('\r'); sys.stdout.write('  {}'.format(info_str)); sys.stdout.flush();
 			if wikioff_graph_node_is_pure(cur, graph, k):
-				ret.append(k)
-	return ret
+				pure.append(k)
+	if progress:
+		print ''
+	if allow_impure_parents == False:
+		fully_pure = []
+		paths = wikioff_graph_root_paths(graph, cat_title)
+		pi = 0
+		pureset = set(pure)
+		for node in pure:
+			node_paths = paths[node]
+			if progress:
+				info_str = '{}/{}. [{} x {}]...'.format(pi+1, len(pure), node, len(node_paths))
+				sys.stdout.write('\x1B[2K'); sys.stdout.write('\r'); sys.stdout.write('  {}'.format(info_str)); sys.stdout.flush();
+			has_a_pure_path = any([all([x in pureset for x in node_path]) for node_path in node_paths])
+			if has_a_pure_path:
+				fully_pure.append(node)
+			pi = pi+1
+		if progress:
+			print ''
+		return fully_pure
+	return pure
 
 
 def wikioff_print_graph_purity(cur, graph, verbose=False):
@@ -166,7 +213,7 @@ def test1():
 		depth_subs = []
 		def get_cat_direct_subs(cat_title):
 			cmd_1 = "select P.page_title from page as P, categorylinks as CL where CL.cl_to='{}' and CL.cl_type='subcat' and P.page_id=CL.cl_from and P.page_namespace=14".format(cat_title.replace("'", "\\'"))
-			cur.execute(cmd_1)
+			wikioff_qry_exec(cur, cmd_1)
 			my_subs = []
 			for row in cur.fetchall():
 				sub_title = row[0]
@@ -174,7 +221,7 @@ def test1():
 				if len(depth_subs) == 2:
 					print 'Checking: ', sub_title
 					cmd_2 = "select CL.cl_to from page as P, categorylinks as CL where P.page_title='{}' and P.page_namespace=14 and CL.cl_from=P.page_id and CL.cl_type='subcat'".format(sub_title.replace("'", "\\'"))
-					cur2.execute(cmd_2)
+					wikioff_qry_exec(cur2, cmd_2)
 					had_rows = False
 					for row in cur2.fetchall():
 						print row
@@ -220,18 +267,28 @@ def wikioff_run_with_conn(lbda):
 
 def test2():
 	cat_title = sys_argv_get(['-cat'], 'Fields_of_mathematics')
-	graph = wikioff_run_with_conn(lambda cur: wikioff_cat_graph_down(cur, cat_title, int(sys_argv_get(['-depth'], 2)), verbose=False, progress=g_progress))
+	max_depth = int(sys_argv_get(['-depth'], 2))
+	half_pure = sys_argv_has(['-half_pure'])
+	graph = wikioff_run_with_conn(lambda cur: wikioff_cat_graph_down(cur, cat_title, max_depth, verbose=False, progress=g_progress))
 	if sys_argv_has(['-purity']):
 		wikioff_run_with_conn(lambda cur: wikioff_print_graph_purity(cur, graph, verbose=g_verbose))
 	else:
 		print ' total nodes: {}'.format(len(graph))
 	if sys_argv_has(['-write']):
+		file_path = '{}_{}'.format(cat_title, max_depth)
 		if sys_argv_has(['-full']):
-			wikioff_graph_to(graph, 'full_'+cat_title, cat_title)
-		pure_nodes = wikioff_run_with_conn(lambda cur: wikioff_graph_pure_nodes(cur, graph))
-		wikioff_graph_to(graph, cat_title, cat_title, pure_nodes)
+			wikioff_graph_to(graph, 'full_'+file_path, cat_title)
+		print ' pure nodes: ',; sys.stdout.flush();
+		if g_progress:
+			print ''
+		pure_nodes = wikioff_run_with_conn(lambda cur: wikioff_graph_pure_nodes(cur, graph, cat_title, half_pure, progress = g_progress))
+		print '{}'.format(len(pure_nodes))
+		wikioff_graph_to(graph, ('half_' if half_pure else '')+ file_path, cat_title, pure_nodes)
 
 #test1()
 #print wikioff_run_with_conn(lambda cur: wikioff_get_cat_edges(cur, 'Fields_of_mathematics'))
 #print wikioff_run_with_conn(lambda cur: wikioff_cat_graph_down(cur, 'Fields_of_mathematics', 2, verbose=g_verbose)), '\n'
 test2()
+
+if g_wikioff_total_queries:
+	print ' total db queries: {}'.format(g_wikioff_total_queries)
